9a310bfc256c4d92a56bdffb05092ad2b2d3c48f
etsuei <eric@townsquaredigitial.com>
2012-06-17 16:17:43 -0400

    test

---
+++ test.php
@@ -0,0 +1,20 @@
+<?php
+
+$post = json_decode($_POST);
+$repo = $post['repository'];
+$repo_name = $repo['name'];
+
+switch ($repo_name) {
+    case 'nycup':
+        exec('cd ~/public_html/git/nycup/');
+        exec('git pull');
+        break;
+    case 'hyc':
+        exec('cd ~/public_html/git/hyc/');
+        exec('git pull');
+        break;
+    case 'tpol':
+        exec('cd ~/public_html/git/tpol/');
+        exec('git pull');
+        break;
+}
